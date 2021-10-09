022356a3283e97e0da78621b209237ceaf7d109f
임준식 <yjs3694@gmail.com>
2021-10-10 00:11:25 +0900

    영화 목록 관련 파싱 로직 및 데이터 처리 로직 추가

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -84,5 +84,35 @@ class ListViewController : UITableViewController {
         // 3. 데이터 전송 결과를 로그로 출력 (반드시 필요한 코드는 아님)
         let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? ""
         NSLog("API Result=\(log)")
+        
+        do {
+            // 4. JSON 객체를 파싱하여 NSDictionary 객체로 받음.
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            
+            // 5. 데이터 구조에 따라 차례대로 캐스팅하며 읽어온다.
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            
+            // 6. Iterator 처리를 하면서 API 데이터를 MovieVO 객체에 저장.
+            for row in movie {
+                // 순회 상수를 NSDictionary 타입으로 캐스팅
+                let r = row as! NSDictionary
+                
+                // 테이블 뷰 리스트를 구성할 데이터 형식
+                let mvo = MovieVO()
+                
+                // movie 배열의 각 데이터를 mvo 상수의 속성에 대입
+                mvo.title           = r["title"] as? String
+                mvo.description     = r["genreNames"] as? String
+                mvo.thumbnail       = r["thumbnailImage"] as? String
+                mvo.detail          = r["linkUrl"] as? String
+                mvo.rating          = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                // list 배열에 추가
+                self.list.append(mvo)
+            }
+        }
+        catch { }
     }
 }
