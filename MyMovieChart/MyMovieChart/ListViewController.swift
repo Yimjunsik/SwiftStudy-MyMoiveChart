995f7280078603c46b31f94f4d783fc5a4a0ab0f
임준식 <yjs3694@gmail.com>
2021-10-10 00:14:10 +0900

    기존의 Dummy 용 샘플 데이터 및 관련 로직 주석 처리

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -9,25 +9,25 @@ import UIKit
 class ListViewController : UITableViewController {
     
     // 튜플 아이템으로 구성된 데이터 세트
-    var dataset = [
-        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpeg"),
-        ("호우 시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpeg"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpeg")
-    ]
+//    var dataset = [
+//        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpeg"),
+//        ("호우 시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpeg"),
+//        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpeg")
+//    ]
     
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO] ()
-        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            mvo.thumbnail = thumbnail
-            
-            datalist.append(mvo)
-        }
+//        for (title, desc, opendate, rating, thumbnail) in self.dataset {
+//            let mvo = MovieVO()
+//            mvo.title = title
+//            mvo.description = desc
+//            mvo.opendate = opendate
+//            mvo.rating = rating
+//            mvo.thumbnail = thumbnail
+//
+//            datalist.append(mvo)
+//        }
         return datalist
     }()
 
